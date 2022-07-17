7d8a8028fbebc93c999a12f89b2d50007a2c3a7d
zhangce <z874010534@vip.qq.com>
2022-07-17 15:57:25 +0800

    feat: useContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 // import Mousetracker from "./components/Mousetracker";
 import LikeButton from "./components/LikeButton";
+
+const theme = {
+  light: {
+    color: 'red'
+  }
+}
+
+export const ThemeContext = React.createContext(theme.light)
 function App() {
   return (
     <div className="App">
-      <LikeButton></LikeButton>
-      {/*<Mousetracker></Mousetracker>*/}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeContext.Provider value={theme.light}>
+        <LikeButton></LikeButton>
+        {/*<Mousetracker></Mousetracker>*/}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </ThemeContext.Provider>
     </div>
   );
 }
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,8 +1,13 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
+import {ThemeContext} from "../App";
 
 const LikeButton: React.FC = () => {
   const [like, setLike] = useState(0)
   const likeRef = useRef(0)
+  const didMountRef = useRef(false)
+  const domRef = useRef<HTMLInputElement>(null)
+  const theme = useContext(ThemeContext)
+  console.log(theme)
   useEffect(() => {
     document.title = `点击了${like}次`
   }, [like])
@@ -12,9 +17,21 @@ const LikeButton: React.FC = () => {
       console.log(likeRef.current)
     }, 3000)
   }
-
+  useEffect(() => {
+    if(didMountRef.current) {
+      console.log('update')
+    }else {
+      didMountRef.current = true
+    }
+  })
+  useEffect(() => {
+    if( domRef.current) {
+      domRef.current.focus()
+    }
+  })
   return (
     <div>
+      <input type="text" ref={domRef}/>
       <button onClick={() => {
         setLike(like + 1);
         likeRef.current = like + 1
